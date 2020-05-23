223468ebc0ee6e874903e8baf099d4427f42b693
Stefan Sicklinger <mail@sicklinger.com>
2020-05-23 14:34:34 +0200

    first model

--- model.py
+++ model.py
@@ -23,11 +23,37 @@ class EncoderCNN(nn.Module):
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
+        ''' Initialize the layers of this model.'''
+        super(LSTMTagger, self).__init__()
+        # embedding layer that turns words into a vector of a specified size
+        self.word_embeddings = nn.Embedding(vocab_size, embed_size)
+        # the LSTM takes embedded word vectors (of a specified size) as inputs 
+        # and outputs hidden states of size hidden_dim
+        self.lstm = nn.LSTM(embed_size, hidden_size)
+        # the linear layer that maps the hidden state output dimension 
+        # to the number of tags we want as output, tagset_size (in this case this is 3 tags)
+        self.hidden2tag = nn.Linear(hidden_size, vocab_size)    
+        # initialize the hidden state (see code below)
+        self.hidden = self.init_hidden()
     
     def forward(self, features, captions):
-        pass
+        ''' Define the feedforward behavior of the model.'''
+        # create embedded word vectors for each word in a sentence
+        embeds = self.word_embeddings(captions)    
+        embeds = torch.cat((features.unsqueeze(1), embeds), 1)
+        lstm_out, _ = self.lstm(packed)
+        tag_outputs  = self.hidden2tag(lstm_out)
+        return tag_outputs
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
-        pass
+        for i in range(max_len):
+            lstm_out, states = self.lstm(inputs, states)
+            tag_output = self.hidden2tag(lstm_out)
+
+            # don't need softmax, since eventually after it argmax will return the same index
+            predicted = torch.argmax(tag_output, dim=-1)
+            result.append(predicted[0,0].item())
+            inputs = self.embed(predicted)
+
+        return result
