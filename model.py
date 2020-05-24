e205c466c74e461eb5820b0b50e90104d3a091d4
Stefan Sicklinger <mail@sicklinger.com>
2020-05-24 23:02:59 +0200

    submit

--- model.py
+++ model.py
@@ -24,36 +24,45 @@ class EncoderCNN(nn.Module):
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
         ''' Initialize the layers of this model.'''
-        super(LSTMTagger, self).__init__()
+        super(DecoderRNN, self).__init__()
         # embedding layer that turns words into a vector of a specified size
         self.word_embeddings = nn.Embedding(vocab_size, embed_size)
         # the LSTM takes embedded word vectors (of a specified size) as inputs 
         # and outputs hidden states of size hidden_dim
-        self.lstm = nn.LSTM(embed_size, hidden_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, dropout = 0,  batch_first=True)
         # the linear layer that maps the hidden state output dimension 
         # to the number of tags we want as output, tagset_size (in this case this is 3 tags)
         self.hidden2tag = nn.Linear(hidden_size, vocab_size)    
-        # initialize the hidden state (see code below)
-        self.hidden = self.init_hidden()
     
     def forward(self, features, captions):
         ''' Define the feedforward behavior of the model.'''
         # create embedded word vectors for each word in a sentence
+        captions = captions[:, :-1] # get rid of <end>
         embeds = self.word_embeddings(captions)    
         embeds = torch.cat((features.unsqueeze(1), embeds), 1)
-        lstm_out, _ = self.lstm(packed)
+        lstm_out, _ = self.lstm(embeds)
         tag_outputs  = self.hidden2tag(lstm_out)
         return tag_outputs
 
     def sample(self, inputs, states=None, max_len=20):
-        " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
-        for i in range(max_len):
-            lstm_out, states = self.lstm(inputs, states)
-            tag_output = self.hidden2tag(lstm_out)
-
-            # don't need softmax, since eventually after it argmax will return the same index
-            predicted = torch.argmax(tag_output, dim=-1)
-            result.append(predicted[0,0].item())
-            inputs = self.embed(predicted)
-
-        return result
+        words = []   
+        words_size = 0
+        
+        while (words_size != max_len+1):
+            # inputs (1,1,embed_size)
+            # output (1,1,hidden_size)
+            output, states = self.lstm(inputs,states)   
+            # output.squeeze(dim = 1) (1,hidden_size)
+            # output (1,vocab_size)
+            output = self.hidden2tag(output.squeeze(dim = 1))
+            _, predicted_index = torch.max(output, 1)
+            # back to host
+            words.append(predicted_index.cpu().numpy()[0].item())   
+            # Look for <end> in the vocabulary 
+            if (predicted_index == 1):
+                break    
+            # Go to next time step
+            inputs = self.word_embeddings(predicted_index)   
+            inputs = inputs.unsqueeze(1) 
+            words_size += 1
+        return words
